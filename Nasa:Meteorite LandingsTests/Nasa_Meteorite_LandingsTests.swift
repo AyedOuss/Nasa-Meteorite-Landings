f3e787e435aabb738683d127410ba456d38ef089
Oussama Ayed <ayed.oussama@gmail.com>
2017-12-12 13:18:17 +0100

    Initial Commit

---
+++ Nasa:Meteorite LandingsTests/Nasa_Meteorite_LandingsTests.swift
@@ -0,0 +1,36 @@
+//
+//  Nasa_Meteorite_LandingsTests.swift
+//  Nasa:Meteorite LandingsTests
+//
+//  Created by Oussama Ayed on 12/12/2017.
+//  Copyright © 2017 Oussama Ayed. All rights reserved.
+//
+
+import XCTest
+@testable import Nasa_Meteorite_Landings
+
+class Nasa_Meteorite_LandingsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
