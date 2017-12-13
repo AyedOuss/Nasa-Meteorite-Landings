1aef196f7b65ac8ef692140c951880727cec5ec2
Oussama Ayed <ayed.oussama@gmail.com>
2017-12-13 14:44:14 +0100

    second commit

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '10.0'
+use_frameworks!
+
+target 'Nasa:Meteorite Landings' do
+    pod 'SwiftyJSON'
+    pod 'SwiftSpinner'
+end
---
+++ Pods/Target Support Files/Pods-Nasa:Meteorite Landings/Pods-Nasa:Meteorite Landings-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Nasa_Meteorite_Landings : NSObject
+@end
+@implementation PodsDummy_Pods_Nasa_Meteorite_Landings
+@end
